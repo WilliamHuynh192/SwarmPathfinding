64f02d440e29891fe8d6da9f704799d161e5972d
Aryah Kannan <68197305+shamone03@users.noreply.github.com>
2024-03-26 18:05:44 -0300

    boiding logic

--- Assets/Scripts/Boid/Boid.cs
+++ Assets/Scripts/Boid/Boid.cs
@@ -17,33 +17,70 @@ namespace Boid {
         [field: SerializeField] public float Speed { private get; set; }
         [SerializeField] private float perception;
 
-        [SerializeField] private Rigidbody rb;
-
+        public Vector3 Acceleration { get; private set; }
+        public Vector3 Velocity { get; private set; }
         private INeighbours _neighbours;
 
         private void Start() {
-            rb.useGravity = false;
+            Multipliers = new Multipliers { Alignment = 1, Separation = 1, Cohesion = 1 };
         }
 
         private void Update() {
-            rb.velocity = GetVelocity(_neighbours.Get(transform.position, perception)) * Time.deltaTime;
+            transform.position += GetVelocity(_neighbours.Get(transform.position, perception)) * Time.deltaTime;
         }
 
         private Vector3 GetVelocity(List<Boid> neighbours) {
-            return Alignment(neighbours) * Multipliers.Alignment + Cohesion(neighbours) * Multipliers.Cohesion + Separation(neighbours) * Multipliers.Separation;
+            return Alignment(neighbours) * Multipliers.Alignment + Cohesion(neighbours) * Multipliers.Cohesion +
+                   Separation(neighbours) * Multipliers.Separation;
         }
 
         private Vector3 Alignment(List<Boid> neighbours) {
-            return Vector3.zero;
+            Vector3 alignment = Vector3.zero;
+
+            foreach (var boid in neighbours) {
+                if (this != boid) {
+                    alignment += boid.Velocity;
+                }
+            }
+
+            if (neighbours.Count > 0) {
+                alignment /= neighbours.Count;
+                alignment -= Velocity;
+                alignment = Vector3.ClampMagnitude(alignment, Speed);
+            }
+
+            return alignment;
         }
 
         private Vector3 Cohesion(List<Boid> neighbours) {
-            return Vector3.zero;
+            var cohesion = Vector3.zero;
+            foreach (var boid in neighbours) {
+                if (this != boid) {
+                    cohesion  += boid.transform.position;
+                }
+            }
+            if (neighbours.Count > 0) {
+                cohesion /= neighbours.Count;
+                cohesion -= transform.position;
+                cohesion = Vector3.ClampMagnitude(cohesion, Speed);
+            }
+            return cohesion;
         }
 
         private Vector3 Separation(List<Boid> neighbours) {
-            return Vector3.zero;
-        }
+            var separation = Vector3.zero;
+            foreach (var boid in neighbours) {
+                if (this != boid) {
+                    var offset = transform.position - boid.transform.position;
+                    separation += offset / offset.sqrMagnitude;
+                }
+            }
 
+            if (neighbours.Count > 0) {
+                separation /= neighbours.Count;
+                separation = Vector3.ClampMagnitude(separation, Speed);
+            }
+            return separation;
+        }
     }
 }
